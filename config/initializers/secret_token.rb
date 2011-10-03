6f437737666c1c0526f6e7ccdaa333089580250c
David Settino <dsetin19@gmail.com>
2011-10-03 16:38:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Netduino::Application.config.secret_token = '78dfa95887bf1a13707646d3ba13e4ff47750c64ff647806583c002d7ebf101faa270be41f9d4589a555f8db507835faac7e4c0d3de2de6b20ff80ae7749ad20'
